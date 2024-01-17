9b6388cf709f8d794a18ac7dda345ec2f3283432
Zaprovic <jazaro2001@gmail.com>
2024-01-17 14:00:07 -0500

    added next auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,33 @@
-import { NextResponse } from "next/server";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import Github from "next-auth/providers/github";
 
-export async function GET(req: Request) {
-	return NextResponse.json({
-		message: "Next auth configured",
-	});
+// export async function GET(req: Request) {
+// 	return NextResponse.json({
+// 		message: "Next auth configured",
+// 	});
+// }
+
+const clientId = process.env.GOOGLE_ID;
+const clientSecret = process.env.GOOGLE_SECRET;
+
+if (!clientId || !clientSecret) {
+	throw new Error("Missing enviroment variables");
 }
+
+const authOptions: AuthOptions = {
+	providers: [
+		Github({
+			clientId,
+			clientSecret,
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET,
+	pages: {
+		signIn: "/login",
+	},
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
