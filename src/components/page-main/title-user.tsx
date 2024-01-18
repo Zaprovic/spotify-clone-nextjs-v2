65bc618b6e8a8981d7dc6b8c97920c51e694d863
Zaprovic <jazaro2001@gmail.com>
2024-01-18 12:13:54 -0500

    added ui to the main content

---
+++ src/components/page-main/title-user.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { ReactNode } from "react";
+
+const TitleUser = ({ children }: { children?: ReactNode }) => {
+    const { data: session } = useSession();
+    const isUserSession = session?.user !== undefined;
+
+    return <>{isUserSession && <span>{session.user?.name}</span>}</>;
+};
+
+export default TitleUser;
