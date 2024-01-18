198f289510e6f4f7b3bf0839e76efbccb34c61f2
Zaprovic <jazaro2001@gmail.com>
2024-01-18 10:42:12 -0500

    added popover content with shadcn to guarantee accesibility

--- src/components/page-main/content/content-header.tsx
+++ src/components/page-main/content/content-header.tsx
@@ -1,7 +1,13 @@
 "use client";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 import InstallIcon from "@/icons/install-icon";
+import More from "@/icons/more";
 import NotificationsIcon from "@/icons/notifications-icon";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import ContentHeaderNavigation from "./content-header-navigation";
@@ -37,17 +43,48 @@ const ContentHeader = () => {
                         <NotificationsIcon />
                     </Link>
 
-                    <button>
-                        <figure className="rounded-full bg-neutral-950 p-1 transition-all hover:scale-105 hover:bg-black">
-                            <Image
-                                src={session.user?.image}
-                                alt="Profile picture"
-                                width={24}
-                                height={24}
-                                className="rounded-full object-cover"
-                            />
-                        </figure>
-                    </button>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <button>
+                                <figure className="rounded-full bg-neutral-950 p-1 transition-all hover:scale-105 hover:bg-black">
+                                    <Image
+                                        src={session.user?.image}
+                                        alt="Profile picture"
+                                        width={24}
+                                        height={24}
+                                        className="rounded-full object-cover"
+                                    />
+                                </figure>
+                            </button>
+                        </PopoverTrigger>
+                        <PopoverContent className="mr-7 mt-1 w-52 rounded-sm border-none bg-neutral-800 p-[0.25rem]">
+                            <ul className="flex flex-col gap-1 text-sm">
+                                <li className="h-full w-full rounded-sm p-2.5 transition-all hover:bg-neutral-500">
+                                    <Link
+                                        href={"#"}
+                                        className="flex justify-between"
+                                    >
+                                        <span>Account</span>
+                                        <More />
+                                    </Link>
+                                </li>
+                                <li className="h-full w-full rounded-sm p-2.5 transition-all hover:bg-neutral-500">
+                                    <Link href={"#"}>Profile</Link>
+                                </li>
+                                <li className="h-full w-full rounded-sm p-2.5 transition-all hover:bg-neutral-500">
+                                    <Link href={"#"}>Settings</Link>
+                                </li>
+                                <hr className="border-neutral-700" />
+                                <li className="h-full w-full rounded-sm p-2.5 transition-all hover:bg-neutral-500">
+                                    <Link href={"#"}>
+                                        <button onClick={() => signOut()}>
+                                            Log out
+                                        </button>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </PopoverContent>
+                    </Popover>
                 </nav>
             ) : (
                 <div className="flex gap-8 font-bold">
